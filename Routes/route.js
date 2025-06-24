579d9626de593e28f6fd10ce886fb30c182a70f2
Raghav <raghavvohra375@gmail.com>
2025-06-24 22:42:05 +0530

    YEs

--- Routes/route.js
+++ Routes/route.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { Signup, DirectLogin, FindEmail, Sendotp, Createuser } from "../Functions/functions.js";
+import { Signup, DirectLogin, FindEmail, Sendotp, Createuser, DeleteUser } from "../Functions/functions.js";
 const router = Router();
 router.post("/login", DirectLogin)
 router.post("/find-user", FindEmail)
 router.post("/send-otp", Sendotp)
 router.post("/signup", Signup)
 router.post("/create-user",Createuser)
-router.post("/delete-user",)
+router.post("/delete-user",DeleteUser)
 export default router;
